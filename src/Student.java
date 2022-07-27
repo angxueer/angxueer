e936a6f8ff08bd8b28761196c0c0825be18e44ef
Your Name <you@example.com>
2022-07-27 11:20:27 +0800

    123

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+//学生类
+public class Student {
+    /**
+     * 学号
+     * 姓名
+     * 年龄
+     * 性别
+     */
+    public String studentNu;
+    public String studentName;
+
+    //行为
+    public void goToNightClasses(){
+        System.out.println("我们在上晚自习");
+    }
+
+    public void goToTheLibrary(){
+        System.out.println("我们在图书馆");
+    }
+    public void goToClass(){
+        System.out.println("我们在上课");
+    }
+    public void doRadioGymnastics(){
+        System.out.println("我们在广播体操");
+    }
+}
+
